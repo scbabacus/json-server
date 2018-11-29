dd607754f8fe7ea8a620b6769d24269f2df8ebe5
Pongthai Chandpoung <pongthai.chandpoung@scbabacus.com>
2018-11-29 07:05:44 -0800

    Build system with gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const md = require('gulp-markdown');
+const rename = require('gulp-rename');
+
+gulp.task('compile-ts', () => {
+  return gulp.src('src/**/*.ts')
+          .pipe(ts('./tsconfig.json'))
+          .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('compile-md', () => {
+  return gulp.src('./README.md')
+    .pipe(md())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["compile-md", "compile-ts"]);
